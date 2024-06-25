9211abddd8cbb60ad06f8c14191a0135c4a672ac
Karlit <karlit@MBP-de-Karlit.lan>
2024-06-25 17:03:12 +0200

    modif page d'accueil

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import '../styles/Home.scss';
 
 
 function Home() {
+    const [dots, setDots] = useState('');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDots(prevDots => {
+                if (prevDots.length >= 3) {
+                    return '';
+                } else {
+                    return prevDots + '.';
+                }
+            });
+        }, 500);
+
+        return () => clearInterval(interval); 
+    }, []);
+
     return (
         <h1 className="home">
-            Faites votre choix !!
+            {dots}
         </h1>
-    )
+    );
 }
 
 export default Home;
--- src/components/Item.js
+++ src/components/Item.js
@@ -6,7 +6,7 @@ function Item({ selectedAlbum }) {
     return (
         <div className='item'>
             <h1>{selectedAlbum.name} ({selectedAlbum.year})</h1>
-            <p>par <em>{selectedAlbum.band}</em></p>
+            <p>par <span>{selectedAlbum.band}</span></p>
             <img src={selectedAlbum.cover} alt={selectedAlbum.name} />
         </div>
     )
